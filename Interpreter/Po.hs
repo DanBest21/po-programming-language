4671d04b2bb338c1953b81a7b177d4c4907a3f48
Giorgio Momigliano <gmomigliano@protonmail.com>
2020-05-03 17:08:04 +0200

    Test fixed function declaration typing issues

--- Interpreter/Po.hs
+++ Interpreter/Po.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main = catch main' noParse
 
 main' = do -- (fileName : _ ) <- getArgs 
-           sourceCode <- readFile "../Source Code/pr2.spl" -- fileName
+           sourceCode <- readFile "../Source Code/pr0.spl" -- fileName
            contents <- readFile "../Tests/p2_input.txt" -- getContents
            let input = seq (checkInput $ words contents) (streamsSplit contents)
            let ast = parse $ alexScanTokens $ sourceCode
